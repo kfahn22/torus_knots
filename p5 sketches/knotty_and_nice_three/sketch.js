6c281ccdf0001a782c40d7e19f6833cf25dccf6d
Kathy <65121394+kfahn22@users.noreply.github.com>
2022-12-16 15:06:21 -0500

    Add sketch

---
+++ p5 sketches/knotty_and_nice_three/sketch.js
@@ -0,0 +1,119 @@
+// Code based on 3D Knot coding challenge
+// Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/challenges/87-3d-knots
+// https://youtu.be/r6YMKr1X0VA
+// Javascript transcription: Chuck England
+
+// Code from challenge: https://editor.p5js.org/codingtrain/sketches/fa1rAWng9
+
+// Equation for Torus knots from
+// https://home.adelphi.edu/~stemkoski/knotgallery/
+
+// https://github.com/davepagurek/p5.Framebuffer
+
+// Adjusting p & q don't affect rendering so no longer truely a torus knot?
+
+let angle = 0;
+let beta = 0;
+let knots = [];
+let p = 9;
+let q = 8;
+let r; // will determine scale of knot
+let h = 3;
+let x, y;
+let num = 15;
+let amt, mr, mr1;
+let adj;
+let ox = 50; // as ox increases height/radius of orbit increases 
+let oy = 0.3; // affects y position of orbit
+let blurRenderer;
+let frames = 361;
+
+let colorOptions = [
+    [0,0,0],
+    [246,245,174],
+    [241,91,181],
+    [0,187,249],
+    [75,47,200] 
+]
+
+function keyPressed() {
+    if (key == "s") {
+        const options = {
+            units: "frames",
+            delay: 0
+        }
+        saveGif("GIF/knotty2.gif", frames, options);
+    }
+}
+
+function setup() {
+    createCanvas(400, 400, WEBGL);
+    angleMode(DEGREES);
+    //https://p5js.org/reference/#/p5/blendMode
+    //blendMode(BLEND);
+    blendMode(REPLACE);
+    //blendMode(SUBTRACT);  not sure why this is not working??
+
+    //lights();
+    // There's a bug in Firefox where you can only make floating point textures
+    // if they're RGBA, and it breaks if it's just RGB
+    //blurRenderer = createGaussianBlurRenderer()
+    blurRenderer = createBlurRenderer();
+    blurRenderer.setIntensity(0.25);
+    blurRenderer.setSamples(10);
+    blurRenderer.setDof(10);
+
+    for (i = 0; i < num; i++) {
+        // adjust radius of tori 
+        mr1 = map(i, 0, num, 0, 0.999);
+        r = i * (1 - mr1);
+
+        mr = map(r, 0, num, height/num, r);
+        x = 0;
+        y = mr;
+        amt = i / num;
+        let c;
+        let one = color(colorOptions[1]);
+        let two = color(colorOptions[2]);
+        let three = color(colorOptions[3]);
+        let four = color(colorOptions[4]);
+        if (amt < 0.333) {
+            c = lerpColor(one, two, amt);
+        } else if (amt > 0.333 & amt < 0.666) {
+            c = lerpColor(two, three, amt);
+        } else {
+            c = lerpColor(three, four, amt);
+        }
+        
+        knots.push(new TorusKnot(x, y, p, q, r, h, c));
+    }
+}
+
+function draw() {
+    translate(0, -height/2, 0); // for spheres
+
+    blurRenderer.draw(() => {
+        clear();
+        push();
+        background(colorOptions[0]);
+        noStroke();
+        push();
+        translate(0, -height / 4, 0);
+        blurRenderer.focusHere();
+        pop();
+        push();
+        for (let i = 0; i < knots.length; i++) {
+            rotateY(angle + i / 90);
+            knots[i].oneKnot();
+            // use .show() for regular torusknot or .showSphere() for spheres 
+            //knots[i].show();
+            knots[i].showSphere();
+            //angle += 0.01;
+            angle -= TWO_PI / frames;
+        }
+        pop();
+        pop();
+    })
+    
+}
